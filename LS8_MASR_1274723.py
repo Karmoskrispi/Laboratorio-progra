93c85cc3c3eed6cc68b551ecde9b0b0ec117e333
Karmoskrispi <143531036+Karmoskrispi@users.noreply.github.com>
2023-09-26 21:36:48 -0600

    laboratorio semana 8 codigo bueno
    este es el laboratorio de la semana 8, este codigo es el bueno

--- LS8_MASR_1274723.py
+++ LS8_MASR_1274723.py
@@ -1,5 +1,4 @@
 codigo = True 
-
 while codigo: 
     print("1 factorial")
     print("2 secuencia de fibonacci")
@@ -8,13 +7,37 @@ while codigo:
 
     if x == 1:
         a = int(input("ingrese el numero a operar= "))
+        factorial = 1
+        c = 1
+        if a < 0:
+            print("elija un numero positivo")
+        elif a >= 0:
+            for i in range(1, a+1):
+                factorial = factorial * c
+                c = c + 1
+                print( a, "x", factorial, "=", factorial)
 
-    if a >= 1: 
-        i = 1
-        for a in range(1,i+1):
-            resultado = (a*(a-1)) 
-            a1 = i * resultado 
-            print("el factorial es= ", a1)
+    elif x == 2:
+        z = int(input("numero de posicion: "))
+        for i in range(1,z):
+          if z < 2:
+                print(z)
+        else:
+            u = ((1+(5)**0.5)/2)
+            j = ((u**z-(1-u)**z)/(5)**0.5)
+            print("la posicion", z, "es",round(j))
+    elif x>3: 
+        print("elija una opcion valida")
+
+    else:
+        x==3 
+        m = input("¿desde salir de las opciones? (si-no):")
+        if m == "si":
+                print("hasta luego")
+                codigo=False
+        else: 
+                m=="no"
+                codigo=True
             
 
 
